086416a209912822da49515226a8036701e5bbf6
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-02-03 16:20:45 +0300

    v1.0.1
    routes üzerinden dashboard tanımlandı

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+/**
+ * Kobiyim
+ * 
+ * @package kobiyim/kobiyim
+ * @since v1.0.1
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@dashboard')->name('dashboard');
