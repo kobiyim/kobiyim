5a1876e9698a7139b7ff34baa706c1681bf665e9
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-02-04 18:52:08 +0300

    v1.0.11
    kobiyim ayar dosyaları dahil edildi

---
+++ config/kobiyim.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Kobiyim
+ * 
+ * @package kobiyim/kobiyim
+ * @since v1.0.11
+ */
+
+return [
+
+    'name'      => env('KOBIYIM_NAME'),  
+
+    'username'  => env('KOBIYIM_USERNAME'),
+
+    'secret'    => env('KOBIYIM_SECRET'),
+
+    'key'       => env('KOBIYIM_KEY'),
+
+];
