a77967ed2ef13ee390b51fbd822b2d1d3cbc5af7
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-02-04 18:34:52 +0300

    v1.0.6
    pagescontroller oluşturuldu

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Kobiyim
+ * 
+ * @package kobiyim/kobiyim
+ * @since v1.0.5
+ */
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+
+    public function dashboard()
+    {
+        return view('welcome');
+    }
+
+}
