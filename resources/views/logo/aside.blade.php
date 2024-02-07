7026d5dde9029ac694c171bb3a28ca33b5bd66c0
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-02-07 18:20:57 +0300

    v1.0.14
    logo isimleri uyarlarndı

--- resources/views/logo/aside.blade.php
+++ resources/views/logo/aside.blade.php
@@ -3,11 +3,11 @@
  * Kobiyim
  * 
  * @package kobiyim/kobiyim
- * @since v1.0.0
+ * @since v1.0.14
  */
 --}}
 <div class="brand-logo">
 	<a href="{{ route('dashboard') }}">
-		<h4 class="text-primary text-hover-secondary" style="margin-bottom: 0px;">{{ config('kobiyim.name', 'KOBIYIM') }}</h4>
+		<h4 class="text-hover-secondary font-weight-bolder" style="margin-bottom: 0px;color: #004c4c;">{{ config('kobiyim.name', 'KOBIYIM') }}</h4>
 	</a>
 </div>
--- resources/views/logo/mobile.blade.php
+++ resources/views/logo/mobile.blade.php
@@ -3,11 +3,11 @@
  * Kobiyim
  * 
  * @package kobiyim/kobiyim
- * @since v1.0.7
+ * @since v1.0.14
  */
 --}}
 <div class="mobile-logo">
 	<a href="{{ route('dashboard') }}">
-		<h4 class="text-warning text-hover-secondary" style="margin-left:15px;margin-bottom: 0px;">{{ env('KOBIYIM_NAME', 'KOBIYIM') }}</h4>
+		<h4 class="text-hover-secondary font-weight-bolder" style="margin-left:15px;margin-bottom: 0px;color: #004c4c;">{{ env('KOBIYIM_NAME', 'KOBIYIM') }}</h4>
 	</a>
 </div>
