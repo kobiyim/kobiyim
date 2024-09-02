31a5241209bb067ec7734d1536f2406f14bdda21
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-09-02 21:05:05 +0300

    v2.0.1

--- resources/views/kobiyim/auth/login.blade.php
+++ resources/views/kobiyim/auth/login.blade.php
@@ -2,7 +2,8 @@
  /**
  * Kobiyim
  * 
- * @version v2.0.0
+ * @package kobiyim/kobiyim
+ * @since v2.0.1
  */
 --}}
 @extends('kobiyim.theme.auth')
@@ -14,20 +15,20 @@
 	<div class="login-signin">
 		@if ($errors->any())
 			<div class="alert alert-danger">
-				<span>{!! $errors->toArray()['message'][0] !!}</span>
+				<span>{!! implode(array_values($errors->all()), ' - ') !!}</span>
 			</div>
 		@endif
 		{{ html()->form()->route('login')->class('form')->open() }}
 			<div class="form-group mb-5">
-				{{ html()->text('phone', null)->attributes([ 'id' => 'phone', 'class' => 'form-control h-auto form-control-solid py-4 px-8', 'autocomplete' => 'off', 'placeholder' => 'Telefon numaranız']) }}
+				{{ html()->text('phone', null)->attributes([ 'id' => 'phone', 'class' => 'form-control h-auto form-control-solid py-4 px-8', 'autocomplete' => 'off', 'placeholder' => 'Telefon numaranız', 'autofocus', 'tabindex' => 1 ]) }}
 			</div>
 			<div class="form-group mb-5">
-				{{ html()->password('password')->attributes([ 'class' => 'form-control h-auto form-control-solid py-4 px-8', 'placeholder' => 'Şifreniz']) }}
+				{{ html()->password('password')->attributes([ 'class' => 'form-control h-auto form-control-solid py-4 px-8', 'placeholder' => 'Şifreniz', 'tabindex' => 2 ]) }}
 			</div>
 			<div class="form-group d-flex flex-wrap justify-content-between align-items-center">
 				<a href="{{ route('password.request') }}" id="kt_login_forgot" class="text-muted text-hover-primary">Şifremi Unuttum?</a>
 			</div>
-			{{ html()->submit('Giriş')->attributes([ 'class' => 'btn btn-primary font-weight-bold px-9 py-4 my-3 mx-4']) }}
+			{{ html()->submit('Giriş')->attributes([ 'class' => 'btn btn-primary font-weight-bold px-9 py-4 my-3 mx-4', 'tabindex' => 3 ]) }}
 		{{ html()->form()->close() }}
 		<div class="mt-10">
 			<span class="opacity-70 mr-4">Hesabınız yok mu?</span>
