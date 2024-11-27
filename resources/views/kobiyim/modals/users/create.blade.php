21b356a6b25eed59e69b1d30d08e1e85d9e419a1
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-11-27 11:48:46 +0300

    v2.0.25
    modal kapatma tıklama ile

--- resources/views/kobiyim/modals/users/create.blade.php
+++ resources/views/kobiyim/modals/users/create.blade.php
@@ -2,10 +2,11 @@
  /**
  * Kobiyim
  * 
- * @version v2.0.0
+ * @package kobiyim/kobiyim
+ * @since v1.0.22
  */
 --}}
-<div class="modal fade" id="createUser" tabindex="-1" role="dialog" aria-labelledby="staticBackdrop" aria-hidden="true">
+<div class="modal fade" id="createUser" data-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="staticBackdrop" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
