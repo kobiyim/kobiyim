a48365d2a246d598075f8da2e2468e26fe3b754d
Nuh <79440046+kobiyim@users.noreply.github.com>
2024-11-29 17:59:29 +0300

    v3.0.5
    proje oluşturma sonrasi bilgilendirme mesajı eklendi

---
+++ app/Console/Commands/KobiyimInform.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Kobiyim
+ *
+ * @version v3.0.5
+ *
+ */
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class KobiyimInform extends Command
+{
+    protected $signature = 'kobiyim:inform';
+
+    protected $description = 'Kobiyim sunar.';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $this->info('Kobiyim e hoşgeldiniz.');
+        $this->info('İsteklerinize özel yönetim panelini hızla ayağa kaldırmaya başlayabilirsiniz.');
+        $this->info('Bu süreçte kobiyim:install komutu ile ilk ayarları yapabilirsiniz.');
+        $this->info('İyi Çalışmalar');
+    }
+}
