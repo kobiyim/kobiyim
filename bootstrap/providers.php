9fcb2bf6d7ff45ba03fffe348ecb3e095ccf455f
Nuh <79440046+kobiyim@users.noreply.github.com>
2025-10-21 17:07:12 +0300

    wip

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,14 +1,6 @@
 <?php
 
-/**
- * Kobiyim
- *
- * @version v3.0.0
- *
- */
-
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    App\Providers\AliasServiceProvider::class
 ];
